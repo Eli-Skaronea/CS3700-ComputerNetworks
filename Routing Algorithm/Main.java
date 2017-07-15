f90f0b458af39dfbacfeb0163a836d1cc828a572
Elijah Skaronea <eli.skaronea@gmail.com>
2017-07-14 19:35:43 -0600

    Vectors and nPrime Working
    Current version will work with 6 routers, need to update if there are less than 6, and clarify what to do if they as for routers not given in txt file. YPrime still needs to be implemented.

--- Routing Algorithm/Main.java
+++ Routing Algorithm/Main.java
@@ -1,9 +1,8 @@
 /**
  * Eli Skaronea
- * Assignment 7 - Routing Algortithm
+ * Assignment 7 - Routing Algorithm
  */
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
@@ -11,40 +10,105 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
+
         int numOfRouters = promptUser();
-        int matrix[][] = new int[numOfRouters][numOfRouters];
-        int nPrime[] = new int[numOfRouters];
-        nPrime[0] = 0;
-        int yPrime[] = new int[numOfRouters];
+        int matrix[][] = readFile(numOfRouters);
 
-        Scanner scan = new Scanner(new File("C:\\Users\\Eli S\\IdeaProjects\\RoutingAlgorithm\\src\\topo.txt"));
-        int row = 0;
-        while (scan.hasNextLine()) {
-            row++;
-            int firstValue = scan.nextInt();
-            int secondValue = scan.nextInt();
-            int cost = scan.nextInt();
-            if (firstValue > numOfRouters - 1 || secondValue > numOfRouters - 1) {
-                System.out.println("There is an invalid router number on row " + row);
-                System.err.println("File has data on router not specified by user");
-                break;
+        int dVector[] = new int[numOfRouters];
+        for (int i = 0; i < numOfRouters; i++) {
+            dVector[i] = matrix[0][i];
+        }
+
+        int pVector[] = new int[numOfRouters];
+        for (int i = 0; i < numOfRouters; i++) {
+            int numCheck = matrix[0][i];
+            if (numCheck == 0 || numCheck == 1000000) {
+                pVector[i] = -1;
             } else {
-                matrix[firstValue][secondValue] = cost;
-                matrix[secondValue][firstValue] = cost;
+                pVector[i] = 0;
             }
-
-
         }
 
-        for(int i = 0; i < numOfRouters; i++)
-        {
-            for(int j = 0; j < numOfRouters; j++)
-            {
-                System.out.printf("%5d ", matrix[i][j]);
+        List<String> nPrime = new ArrayList<>();
+        nPrime.add("V0");
+        List<Integer> checkedIndexes = new ArrayList<>();
+        checkedIndexes.add(0);
+
+        List<String> yPrime = new ArrayList<>();
+
+        int numToCheck = dVector.length - 1;
+        int count = 1;
+
+        while (numToCheck > 0) {
+            int smallestFound = -1;
+
+
+            int index = 0;
+            for (int i = 1; i < numOfRouters; i++) {
+
+                if (!checkedIndexes.contains(i)) {
+                    int cost = dVector[i];
+                    if ((cost < smallestFound && cost != -1) || smallestFound == -1) {
+                        smallestFound = cost;
+                        index = i;
+                    }
+                }
             }
-            System.out.println();
+            numToCheck--;
+            dVector[index] = smallestFound;
+            //pVector[index] = index;
+            nPrime.add(", V" + index);
+            checkedIndexes.add(index);
+            //YPRIME UPDATED
+            for (int i = 0; i < numOfRouters; i++) {
+                int newLinkCost = smallestFound + matrix[index][i];
+                if (newLinkCost < dVector[i] && i != index) {
+                    dVector[i] = newLinkCost;
+                    pVector[i] = index;
+                }
+            }
+
+            System.out.println("ITERATION: " + count);
+            outputInfo(numOfRouters, dVector, pVector, nPrime, yPrime);
+            count++;
         }
 
+
+    }
+
+    public static void outputInfo(int numOfRouters, int dVector[], int pVector[], List<String> nPrime, List<String> yPrime) {
+        System.out.print("D Vector: ");
+        for (int i = 0; i < numOfRouters; i++) {
+            if (dVector[i] == 1000000) {
+                System.out.print("Infinity ");
+            } else {
+                System.out.print(dVector[i] + " ");
+            }
+        }
+        System.out.println("");
+
+        System.out.print("P Vector: ");
+        for (int i = 0; i < numOfRouters; i++) {
+            if (pVector[i] == -1) {
+                System.out.print("- ");
+            } else {
+                System.out.print(pVector[i] + " ");
+            }
+
+        }
+        System.out.println("");
+
+        System.out.print("N Prime: ");
+        for (int i = 0; i < nPrime.size(); i++) {
+            System.out.print(nPrime.get(i));
+        }
+        System.out.println("");
+
+        System.out.print("Y Prime: ");
+        for (int i = 0; i < yPrime.size(); i++) {
+            System.out.print(yPrime.get(i) + ", ");
+        }
+        System.out.println("");
 
     }
 
@@ -57,5 +121,48 @@ public class Main {
         }
 
         return numOfRouters;
+    }
+
+    public static int[][] readFile(int numOfRouters) throws FileNotFoundException {
+        int matrix[][] = new int[numOfRouters][numOfRouters];
+        Scanner scan = new Scanner(new File("C:\\Users\\Eli S\\IdeaProjects\\RoutingAlgorithm\\src\\topo.txt"));
+        int row = 0;
+        while (scan.hasNextLine()) {
+            row++;
+            int firstValue = scan.nextInt();
+            int secondValue = scan.nextInt();
+            int cost = scan.nextInt();
+
+            if (firstValue > numOfRouters - 1 || secondValue > numOfRouters - 1) {
+                System.out.println("There is an invalid router number on row " + row);
+                System.err.println("File has data on router not specified by user");
+                break;
+            } else {
+                matrix[firstValue][secondValue] = cost;
+                matrix[secondValue][firstValue] = cost;
+            }
+            for (int i = 0; i < numOfRouters; i++) {
+                for (int j = 0; j < numOfRouters; j++) {
+                    if (i == j) {
+                        matrix[i][j] = 0;
+                    } else if (matrix[i][j] == 0) {
+                        matrix[i][j] = 1000000;
+                    }
+                }
+            }
+
+
+        }
+
+//        for(int i = 0; i < numOfRouters; i++)
+//        {
+//            for(int j = 0; j < numOfRouters; j++)
+//            {
+//                System.out.printf("%5d ", matrix[i][j]);
+//            }
+//            System.out.println();
+//        }
+
+        return matrix;
     }
 }
